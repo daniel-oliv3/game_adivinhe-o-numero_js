edaecb3dc7c619c59cfb84b894254ec8fa91fd28
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-01-09 10:32:15 -0300

    Update

---
+++ js/script.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+    var numeromisterioso = Math.floor(Math.random() * 101);
+    var chuteDoJogador = 0;
+    var tentativasRestantes = 10;
+    var tentativas = 0;
+    var mensagemAoJogador = "";
+    var vitoria = false;
+
+    var entrada = document.querySelector("#entrada");
+}
