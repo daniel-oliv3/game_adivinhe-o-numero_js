55d65ae195ea4ff582dcd105ab523fe887073988
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-01-14 11:44:41 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 window.onload = function(){
+    //Variáveis
     var numeroMisterioso = Math.floor(Math.random() * 101);
     //alert(numeroMisterioso);
     var chuteDoJogador = 0;
@@ -7,14 +8,20 @@ window.onload = function(){
     var mensagemAoJogador = "";
     var vitoria = false;
 
+    //Elementos interativos
     var entrada = document.querySelector("#entrada");
     var saida = document.querySelector("#saida");
     var ponteiro = document.querySelector("#ponteiro");
-
     var btn = document.querySelector("button");
 
+    //Função atribuída ao elemento button
     btn.addEventListener("click", clickFunction);
 
+    function clickFunction(){
+        validarJogada();
+    }
+
+    //Função atribuída ao teclado (tecla enter)
     window.addEventListener("keydown", keydownFunction);
 
     function keydownFunction(e){
@@ -23,17 +30,14 @@ window.onload = function(){
         }
     }
 
-    function clickFunction(){
-        validarJogada();
-    }
-
+   
 
     function validarJogada(){
         chuteDoJogador = parseInt(entrada.value);
         entrada.value = "";
         entrada.focus();
 
-        if(isNaN(chuteDoJogador)){
+        if(isNaN(chuteDoJogador)){ //o comando isNaN() retorna true se o valor não for um número e false se for um número
             saida.innerHTML = "Por favor, digite apenas números.";
         }else if(chuteDoJogador > 100 || chuteDoJogador < 0){
             saida.innerHTML = "Por favor, digite apenas valores entre 0 e 100.";
@@ -69,18 +73,21 @@ window.onload = function(){
     }
 
     function fimDoJogo(){
-        if(vitoria){
+        if(vitoria){ //Estruturas condicionais trabalham com valores true ou false; então dá na mesma usar if(vitoria) e if(vitoria == true)
             saida.innerHTML = "Parabéns, você adivinhou! o número era " + numeroMisterioso + "<br>"
              + "Foram feitas " + tentativas + " tentativas";
         }else {
             saida.innerHTML = "Acabaram suas tentativas. <br> O núero era " + numeroMisterioso;
         }
+
+        //desabilitamos os elementos de interação com o usuário
         btn.removeEventListener("click", clickFunction);
         window.removeEventListener("keydown", keydownFunction);
         btn.disabled = true;
         entrada.disabled = true;
     }
 
+    //função responsável pela movimentação do ponteiro
     function render(){
        ponteiro.style.left = (chuteDoJogador * 3.7) + 13 + "px";
     }
